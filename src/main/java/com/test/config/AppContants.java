11f4cad84013935f909e8e4ec9697a4f93957288
Alisha <alishatayade@example.com>
2024-07-20 16:21:09 +0530

    trail

--- src/main/java/com/test/config/AppContants.java
+++ src/main/java/com/test/config/AppContants.java
@@ -1,7 +1,7 @@
 package com.test.config;
 
 public class AppContants {
-//bug fix-131 changes
+//bug fix-131 changeges 
 	public static final String PAGE_NUMBER="0";
 	public static final String PAGE_SIZE="5";//MAKE CONTANTS
 	public static final String SORT_BY="post_Id";
