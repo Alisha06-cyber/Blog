13a78f7ba155ceaf0bb80383e9c1dcae90c001b2
Alisha <alishatayade@example.com>
2024-07-20 14:25:47 +0530

    bugfixed131

--- src/main/java/com/test/config/AppContants.java
+++ src/main/java/com/test/config/AppContants.java
@@ -1,7 +1,7 @@
 package com.test.config;
 
 public class AppContants {
-//helloo
+//bug fix-131 changes
 	public static final String PAGE_NUMBER="0";
 	public static final String PAGE_SIZE="5";//MAKE CONTANTS
 	public static final String SORT_BY="post_Id";
